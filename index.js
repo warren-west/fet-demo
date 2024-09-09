6a64d2aa5c3075704b61ae9f9450a26937be0a38
warren-west <teacher.warren.english@gmail.com>
2024-09-09 13:43:49 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// camelCasing: btnShowHide
+// PascalCasing: BtnShowHide
+// kebab-casing: btn-show-hide
+// snake_casing: btn_show_hide
+// uppercase_snake_casing: BTN_SHOW_HIDE
+
+
+let isShowing = true
+const btnShowHide = document.getElementById("btn-show-hide")
+const imgCat = document.getElementById("img-cat")
+
+function toggle() {
+    console.log("Button was clicked!")
+    console.log("isShowing:", isShowing)
+
+    isShowing = !isShowing
+
+    if (isShowing) {
+        imgCat.style.display = "inline" // different options: block, inline, none
+        btnShowHide.innerText = "Hide!"
+        console.log("Show picture")
+    } else {
+        imgCat.style.display = "none" // different options: block, inline, none
+        btnShowHide.innerText = "Show!"
+        console.log("Hide picture")
+    }
+    // console.log(btnShowHide)
+    // console.log(imgCat)
+}
+
+
+// ternary operator -> ? :
+function toggle2() {
+    isShowing = !isShowing
+    imgCat.style.display = isShowing ? "inline" : "none"
+    btnShowHide.innerText = isShowing ? "Hide!" : "Show!"
+}
