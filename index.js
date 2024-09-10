f1ca09cd957d19df3e501321fae6297cdc4113aa
warren-west <teacher.warren.english@gmail.com>
2024-09-10 12:44:11 +0200

    Added html table, and styling demo.

--- index.js
+++ index.js
@@ -4,10 +4,14 @@
 // snake_casing: btn_show_hide
 // uppercase_snake_casing: BTN_SHOW_HIDE
 
+// DOM = Document Object Model
+// DOM traversal
+// DOM manipulation
 
 let isShowing = true
 const btnShowHide = document.getElementById("btn-show-hide")
 const imgCat = document.getElementById("img-cat")
+const divContainerElement = document.getElementById("container")
 
 function toggle() {
     console.log("Button was clicked!")
@@ -34,4 +38,8 @@ function toggle2() {
     isShowing = !isShowing
     imgCat.style.display = isShowing ? "inline" : "none"
     btnShowHide.innerText = isShowing ? "Hide!" : "Show!"
+}
+
+function hideContainer() {
+    divContainerElement.style.display = divContainerElement.style.display == "none" ? "block" : "none"
 }
